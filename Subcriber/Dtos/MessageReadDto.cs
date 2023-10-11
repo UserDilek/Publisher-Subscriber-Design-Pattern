c53a6dac23e935c6b0ab2d3e0a3f201b79d33544
ozgurkara <Dhocaoglukara9037@conestogac.on.ca>
2023-10-11 12:31:18 -0400

    Subscriber added

---
+++ Subcriber/Dtos/MessageReadDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Subcriber.Dtos
+{
+	public class MessageReadDto
+	{
+		            
+        public int ID { get; set; }
+           
+        public string? TopicMessage { get; set; }
+
+        public DateTime ExpiresAfter { get; set; }
+        
+        public string? MessageStatus { get; set; } 
+   
+	}
+}
---
+++ Subcriber/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Subcriber.Dtos;
+using System.Net.Http.Json;
+
+Console.WriteLine("Press Esc to stop");
+
+do
+{
+
+    HttpClient client = new HttpClient();
+    Console.WriteLine("Listening....");
+    while (!Console.KeyAvailable)
+    {
+        List<int> ackIds = await GetMessageAsync(client);
+        Thread.Sleep(3000);
+        if(ackIds.Count > 0) {
+            await AckMessageAsync(client, ackIds);
+        }
+    }
+
+
+
+
+
+} while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+
+
+
+static async Task<List<int>> GetMessageAsync(HttpClient httpClient) {
+
+    List<int> ackIds = new List<int>();
+    List<MessageReadDto>? newMessages = new List<MessageReadDto>();
+
+    try
+    {
+        newMessages = await httpClient.GetFromJsonAsync<List<MessageReadDto>>("localhost");
+             
+    }
+    catch (Exception ex)
+    {
+        return ackIds;
+
+    }
+
+    foreach (var msg in newMessages)
+    {
+        Console.WriteLine($"{msg.ID} - {msg.TopicMessage} - {msg.MessageStatus}");
+        ackIds.Add(msg.ID);
+
+
+    }
+    return ackIds;
+
+}
+
+static async Task AckMessageAsync(HttpClient httpClient,List<int> ackIds) {
+
+    var response = await httpClient.PostAsJsonAsync("https://localhost",ackIds);
+    var returnMessage = await response.Content.ReadAsStringAsync();
+
+    Console.WriteLine(returnMessage);
+
+
+}
